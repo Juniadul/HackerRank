ecd837976a1baf33ec15416c99099ba2399c8491
Md. Juniadul Islam <anikmym500@gmail.com>
2023-09-06 16:21:15 +0600

    first commit

---
+++ nput and Output.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+   int n,a,b,c;
+   cin>>a>>b>>c;
+   n= a+b+c;
+   cout<<n<<endl;
+   
+    return 0;
+}
